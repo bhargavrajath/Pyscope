4ccb9dec03b9fc67e69fd5d30d634cf33522db9c
Rajath Bhargav <74972779+rajathbhargav@users.noreply.github.com>
2021-01-11 16:24:59 +0530

    Update serialcom.py
    Updated comments

--- serialcom.py
+++ serialcom.py
@@ -1,17 +1,19 @@
 # This code reads serial data from a USB port and stores it in a file
+# Author: Rajath Bhargav
 
 import serial as ser
 import datetime as dt
 
+# configure and open port
 port = ser.Serial(port='/dev/ttyACM0',baudrate=115200)
 
 with open('waveform.txt','w+') as fout:
 	fout.write('\n')
-	fout.write(str(dt.datetime.now()))
+	fout.write(str(dt.datetime.now())) # date and time stamp
 	fout.write('\n')
 	count = 0
 
-	while count < 10000:
+	while count < 10000:	# read 100000 bytes
 		data = port.read(1)
-		fout.write(data.decode("UTF-8"))
+		fout.write(data.decode("UTF-8")) # decode UTF-8 to ASCII
 		count = count + 1
