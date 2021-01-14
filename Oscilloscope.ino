cc6ed07c3d3ca37d855b05839aa169c712952778
Rajath Bhargav <74972779+rajathbhargav@users.noreply.github.com>
2021-01-14 17:27:46 +0530

    Updated comments

--- Oscilloscope.ino
+++ Oscilloscope.ino
@@ -1,3 +1,7 @@
+#define ref_volt 3.3  // MCU ADC reference voltage
+#define ADC_res 10    // ADC resolution in bits
+#define ch1 A3        // pin used for analog measurement
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -5,5 +9,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(3.3*analogRead(A3)/1024);
+  Serial.println(ref_volt*analogRead(ch1)/ADC_res);
+  delay(10); // adjust this if you see latency
 }
